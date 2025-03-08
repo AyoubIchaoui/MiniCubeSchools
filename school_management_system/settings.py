fbe6075f5c3fb2fee27d7c4ac690e8e2eb0b5039
AyoubIchaoui <93123824+AyoubIchaoui@users.noreply.github.com>
2025-03-08 12:35:35 +0100

    Update settings.py

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -99,8 +99,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Serve media files during development
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

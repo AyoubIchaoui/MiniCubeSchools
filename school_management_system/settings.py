cdca2954e9f2b71aef82f49346f51715c322630a
AyoubIchaoui <93123824+AyoubIchaoui@users.noreply.github.com>
2025-03-08 12:29:05 +0100

    Update settings.py

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -102,11 +102,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'school/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
-
-
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
-
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
